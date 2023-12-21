2238b8340dfa3209d426037dc5848ecba63ecce0
GorbachevaSasha <alexandragorbachewa@yandex.ru>
2023-12-21 17:01:47 +0500

    Initial Commit

---
+++ mydb.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+dataBase = mysql.connector.connect(
+    host='localhost',
+    user='root',
+    passwd='Itachi133!'
+
+)
+
+cursorObject = dataBase.cursor()
+
+cursorObject.execute("CREATE DATABASE crmdb")
+
+print("All Done!")
